a2dec3718a1dc22f9eb40bad8dd8c53b3608cbd9
Fabien Schmitt <fab.schmitt@hotmail.com>
2025-10-28 00:12:13 +0100

    and one more

--- script/cell.gd
+++ script/cell.gd
@@ -4,6 +4,7 @@ class_name Cell
 @export var replication_speed := 2.0
 @export var size := 50
 @export var max_size := 100
+@export var swarm_factory : SwarmFactory
 
 @onready var size_label : Label = $Label
 @onready var _selected_circle : Sprite2D  = %SelectedCircle
@@ -49,7 +50,7 @@ func on_click():
 func attack(target: Cell):
 	var swarm_size = size/2
 	size -= swarm_size
-	var swarm = SwarmFactory.create_swarm(self, target, swarm_size)
+	var swarm = swarm_factory.create_swarm(self, target, swarm_size)
 	get_tree().current_scene.add_child(swarm)
 	size_label.text = str(size)
 
