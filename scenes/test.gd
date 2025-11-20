df324efc32cd5aaece85d9388d64a8f378059a71
FabienSchmitt <fab.schmitt@hotmail.com>
2025-11-20 22:26:03 +0100

    cleaning + renaming

--- scenes/test.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	var r = Species.new()
-	r.export()
--- scripts/SwarmScene.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	GameManager.create_grid()
-
-func get_obstacle_cells() -> Array[UniformGridCell]:
-	return []
